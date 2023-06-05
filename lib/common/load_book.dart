ca07229611c0b09e66227122e2b9aff6e284bb17
lanyu <abc262@foxmail.com>
2023-06-05 22:24:11 +0800

    ebupview测试

--- lib/common/load_book.dart
+++ lib/common/load_book.dart
@@ -1,80 +1,80 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:epub_view/epub_view.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:epub_view/epub_view.dart';
+// import 'package:file_picker/file_picker.dart';
+// import 'package:flutter/material.dart';
+// // ignore: depend_on_referenced_packages
+// import 'package:path/path.dart';
+// import 'package:path_provider/path_provider.dart';
 
-class BookshelfWidget extends StatefulWidget {
-  const BookshelfWidget({super.key});
+// class BookshelfWidget extends StatefulWidget {
+//   const BookshelfWidget({super.key});
 
-  @override
-  // ignore: library_private_types_in_public_api
-  _BookshelfWidgetState createState() => _BookshelfWidgetState();
-}
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _BookshelfWidgetState createState() => _BookshelfWidgetState();
+// }
 
-class _BookshelfWidgetState extends State<BookshelfWidget> {
-  final List<File> _books = [];
+// class _BookshelfWidgetState extends State<BookshelfWidget> {
+//   final List<File> _books = [];
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: _addBook,
-            ),
-          ],
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _books.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text(_books[index].path),
-                onTap: () {
-                  _openBook(_books[index]);
-                },
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             IconButton(
+//               icon: const Icon(Icons.add),
+//               onPressed: _addBook,
+//             ),
+//           ],
+//         ),
+//         Expanded(
+//           child: ListView.builder(
+//             itemCount: _books.length,
+//             itemBuilder: (BuildContext context, int index) {
+//               return ListTile(
+//                 title: Text(_books[index].path),
+//                 onTap: () {
+//                   _openBook(_books[index]);
+//                 },
+//               );
+//             },
+//           ),
+//         ),
+//       ],
+//     );
+//   }
 
-  void _addBook() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-    if (result != null) {
-      File file = File(result.files.single.path!);
-      await _copyBook(file);
-      setState(() {
-        _books.add(file);
-      });
-    }
-  }
+//   void _addBook() async {
+//     FilePickerResult? result = await FilePicker.platform.pickFiles();
+//     if (result != null) {
+//       File file = File(result.files.single.path!);
+//       await _copyBook(file);
+//       setState(() {
+//         _books.add(file);
+//       });
+//     }
+//   }
 
-  void _openBook(File book) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) => EpubView(
-          filePath: book.path, controller: null,
-        ),
-      ),
-    );
-  }
+//   void _openBook(File book) {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (BuildContext context) => EpubView(
+//           filePath: book.path, controller: null,
+//         ),
+//       ),
+//     );
+//   }
 
-  Future<void> _copyBook(File file) async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String appDocPath = appDocDir.path;
-    String fileName = basename(file.path);
-    String newPath = '$appDocPath/$fileName';
-    await file.copy(newPath);
-  }
-}
+//   Future<void> _copyBook(File file) async {
+//     Directory appDocDir = await getApplicationDocumentsDirectory();
+//     String appDocPath = appDocDir.path;
+//     String fileName = basename(file.path);
+//     String newPath = '$appDocPath/$fileName';
+//     await file.copy(newPath);
+//   }
+// }
--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'components/seach.dart';
 import 'components/infinite_list.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:simple_permissions/simple_permissions.dart';//记得加上这句话
+import 'package:path_provider/path_provider.dart';
+import 'package:epub_view/epub_view.dart';
+
+// import '../../common/test.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,6 +31,7 @@ class HomePage extends StatelessWidget {
                     splashRadius: 20.0,
                     onPressed: () {
                       // TODO: 添加本地 epub 书籍的逻辑
+                      // initState();
                     },
                   ),
                 ],
@@ -36,3 +43,23 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+//读取文件方法
+readData() async {
+  try {
+    //申请读文件的权限
+    var permission =
+    SimplePermissions.requestPermission(Permission.ReadExternalStorage);
+    var sdCardPath = getExternalStorageDirectory();
+    //当获取到路径的时候
+    sdCardPath.then((filePath) {
+      //获得读取文件权限
+      permission.then((permission_status) async {
+        //获取文件内容
+        // var data = await File(filePath.path + "/flutter.txt").readAsString();
+      });
+    });
+  // ignore: empty_catches
+  } catch (e) {}
+}
+
