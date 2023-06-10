8fa32afe1a77dd2db123d52364ee99d83389a3d1
lanyu <abc262@foxmail.com>
2023-06-10 16:45:39 +0800

    StatelessWidget学习

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: const [
-        BookItem('三体', '刘慈欣', 'https://img3.doubanio.com/view/subject/l/public/s2768378.jpg'),
+        BookItem('三体', '刘慈欣', 'https://img1.doubanio.com/view/subject/s/public/s2768378.jpg'),
+        BookItem('寻找百忧解', '陈百忧', 'https://img2.doubanio.com/view/subject/s/public/s34415563.jpg'),
+        BookItem('芯片战争', '[美]克里斯·米勒', 'https://img2.doubanio.com/view/subject/s/public/s34500182.jpg'),
       ],
     );
   }
@@ -41,12 +43,25 @@ class BookItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(title),
-        Text('作者: $author'),
-        Image.network(imgUrl),
-      ],
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: Colors.grey
+          )
+        )
+      ),
+      child: Column(
+        children: [
+          Text(title),
+          Text('作者: $author'),
+          const SizedBox(height: 10,),
+          Image.network(imgUrl),
+          const SizedBox(height: 10,),
+        ],
+      )
     );
   }
 }
