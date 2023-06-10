4b0e08fe65f61e5f7860425d3b072bef7bed4bb0
lanyu <abc262@foxmail.com>
2023-06-10 15:47:30 +0800

    初始页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,202 +1,52 @@
-// import 'package:flutter/material.dart';
-// import 'page/home/home.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Epub Reader',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: HomePage(),
-//     );
-//   }
-// }
-import 'dart:io';
-
-import 'package:device_info_plus/device_info_plus.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:vocsy_epub_viewer/epub_viewer.dart';
 
-void main() async {
-  runApp(MyApp());
-}
+main() {() => runApp(const MyApp());}
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool loading = false;
-  Dio dio = Dio();
-  String filePath = "";
-
-  @override
-  void initState() {
-    download();
-    super.initState();
-  }
-
-  download() async {
-    if (Platform.isAndroid || Platform.isIOS) {
-      String? firstPart;
-      final deviceInfoPlugin = DeviceInfoPlugin();
-      final deviceInfo = await deviceInfoPlugin.deviceInfo;
-      final allInfo = deviceInfo.data;
-      if (allInfo['version']["release"].toString().contains(".")) {
-        int indexOfFirstDot = allInfo['version']["release"].indexOf(".");
-        firstPart = allInfo['version']["release"].substring(0, indexOfFirstDot);
-      } else {
-        firstPart = allInfo['version']["release"];
-      }
-      int intValue = int.parse(firstPart!);
-      if (intValue >= 13) {
-        await startDownload();
-      } else {
-        if (await Permission.storage.isGranted) {
-          await Permission.storage.request();
-          await startDownload();
-        } else {
-          await startDownload();
-        }
-      }
-    } else {
-      loading = false;
-    }
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Epub Reader',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Vocsy Plugin E-pub example'),
+          title: const Text('Epub Reader'),
         ),
-        body: Center(
-          child: loading
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    Text('Downloading.... E-pub'),
-                  ],
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        print("=====filePath======$filePath");
-                        if (filePath == "") {
-                          download();
-                        } else {
-                          VocsyEpub.setConfig(
-                            themeColor: Theme.of(context).primaryColor,
-                            identifier: "iosBook",
-                            scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
-                            allowSharing: true,
-                            enableTts: true,
-                            nightMode: true,
-                          );
-
-                          // get current locator
-                          VocsyEpub.locatorStream.listen((locator) {
-                            print('LOCATOR: $locator');
-                          });
-
-                          VocsyEpub.open(
-                            filePath,
-                            lastLocation: EpubLocator.fromJson({
-                              "bookId": "2239",
-                              "href": "/OEBPS/ch06.xhtml",
-                              "created": 1539934158390,
-                              "locations": {
-                                "cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"
-                              }
-                            }),
-                          );
-                        }
-                      },
-                      child: Text('Open Online E-pub'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        VocsyEpub.setConfig(
-                          themeColor: Theme.of(context).primaryColor,
-                          identifier: "iosBook",
-                          scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
-                          allowSharing: true,
-                          enableTts: true,
-                          nightMode: true,
-                        );
-                        // get current locator
-                        VocsyEpub.locatorStream.listen((locator) {
-                          print('LOCATOR: $locator');
-                        });
-                        await VocsyEpub.openAsset(
-                          '/Books/The.epub',
-                          // lastLocation: EpubLocator.fromJson({
-                          //   "bookId": "2239",
-                          //   "href": "/OEBPS/ch06.xhtml",
-                          //   "created": 1539934158390,
-                          //   "locations": {
-                          //     "cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"
-                          //   }
-                          // }),
-                        );
-                      },
-                      child: Text('Open Assets E-pub'),
-                    ),
-                  ],
-                ),
-        ),
+        body: const HomeContent(),
       ),
     );
   }
+}
 
-  startDownload() async {
-    Directory? appDocDir = Platform.isAndroid
-        ? await getExternalStorageDirectory()
-        : await getApplicationDocumentsDirectory();
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
 
-    String path = appDocDir!.path + '/sample.epub';
-    File file = File(path);
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        BookItem('三体', '刘慈欣', 'https://img3.doubanio.com/view/subject/l/public/s2768378.jpg'),
+      ],
+    );
+  }
+}
 
-    if (!File(path).existsSync()) {
-      await file.create();
-      await dio.download(
-        "https://vocsyinfotech.in/envato/cc/flutter_ebook/uploads/22566_The-Racketeer---John-Grisham.epub",
-        path,
-        deleteOnError: true,
-        onReceiveProgress: (receivedBytes, totalBytes) {
-          setState(() {
-            loading = true;
-          });
-        },
-      ).whenComplete(() {
-        setState(() {
-          loading = false;
-          filePath = path;
-        });
-      });
-    } else {
-      setState(() {
-        loading = false;
-        filePath = path;
-      });
-    }
+class BookItem extends StatelessWidget {
+  final String title;
+  final String author;
+  final String imgUrl;
+  
+  const BookItem(this.title,this.author,this.imgUrl,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(title),
+        Text('作者: $author'),
+        Image.network(imgUrl),
+      ],
+    );
   }
 }
